9a2b10db809a797f7af150bce20a4b4179c0835d
Ezhil <uibuddha@gmail.com>
2025-02-06 09:56:17 +0800

    Use deepseek as default model

--- app/utils/llm_client_service.py
+++ app/utils/llm_client_service.py
@@ -9,7 +9,7 @@ from anthropic import AnthropicVertex
 from vertexai.generative_models import GenerativeModel
 
 def call_llm(messages, response_model: Any):
-        return call_llm_anthropic(messages, response_model)
+        return call_llm_deepseek(messages, response_model)
     
 def call_llm_deepseek(messages, response_model: Any, 
                         model = 'deepseek-chat', max_tokens=8000):
